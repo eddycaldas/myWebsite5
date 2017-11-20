4b80d9a6d13cd1d470fcd97d77bfac976482436b
Eddy Caldas <eddycaldas@live.com>
2017-11-19 19:03:14 -0700

    init website

---
+++ app.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+//   var options = [
+//   // {selector: '.class', offset: 200, callback: customCallbackFunc },
+//   {selector: '.conteudo', offset: 0, callback: function() {
+//     Materialize.toast("I'm a toast", 999000);
+//   } },
+// ];
+// Materialize.scrollFire(options);
+// })
